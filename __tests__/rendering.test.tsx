c8ef928ff2d6f499905fa9a3eb84b8427c290c7d
yon-s <shuichi.yonehara@gmail.com>
2023-12-15 20:55:36 +0900

    [add] ページタイトルの追加 [理由]ページタイトル表示のため

--- __tests__/rendering.test.tsx
+++ __tests__/rendering.test.tsx
@@ -5,15 +5,20 @@ import { render,screen} from "@testing-library/react";
 import React from 'react';
 
 import Header from "../components/Header";
+import MainTitle from "../components/title/MainTitle"
+
 
 declare module "expect" {
   interface Matchers<R extends Promise<void> | void>
     extends TestingLibraryMatchers<typeof expect.stringContaining, R> {}
 }
 
-describe("Header", () => {
+describe("Rendering", () => {
     it("コンポーネントのレンダリングのテスト", async () => {
       render(<Header />);
-      expect(screen.getByText('Habit Todo')).toBeInTheDocument()
+      expect(screen.getByText('Habit Todo')).toBeInTheDocument();
+
+      render(<MainTitle title="TODAY" />);
+      expect(screen.getByText('TODAY')).toBeInTheDocument();
     });
 });
---
+++ components/title/MainTitle.tsx
@@ -0,0 +1,10 @@
+"use client"
+import { Maintitle } from "@/styles/title"
+
+const MainTitle = ({title}:{title:string}) => {
+  return(
+    <Maintitle>{title}</Maintitle>
+  )
+}
+
+export default MainTitle;
